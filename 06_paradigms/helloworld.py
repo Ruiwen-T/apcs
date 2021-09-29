ac0f461896791a68cc824e241633b74c95fbc16b
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2021-09-29 06:01:12 -0400

    Add files via upload

---
+++ 06_paradigms/helloworld.py
@@ -0,0 +1,6 @@
+#Raven (Ruiwen) Tang
+#APCS
+#HW06 -- printing in different languages
+#2021-09-29
+
+print("Hello, world!");
---
+++ 06_paradigms/helloworld.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+#|
+Raven (Ruiwen) Tang
+APCS
+HW06 -- printing in different languages
+2021-09-29
+|#
+
+"Hello, world!"
+
