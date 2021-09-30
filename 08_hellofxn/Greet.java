292f00cb4a98546e9857165fca0ee277dc297270
Ruiwen-T <you@example.com>
2021-09-30 13:15:46 -0400

    initsetup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,15 @@
+//Long Island Railroad: Lawrence Joa, Ivina Wang, Raven (Ruiwen) Tang
+//APCS
+//HW08 - Refactor Freshie Zero
+//2021-09-30
+
+public class Greet {
+        public static void main(String[] args){
+                System.out.println("No hablo queso.");
+		greet("hi");
+        }
+	public static void greet(String s){
+		System.out.println(s);
+	}
+
+}
