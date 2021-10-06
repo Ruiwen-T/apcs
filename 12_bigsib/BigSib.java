02f49f82444f1a147475d8927670c75560a87600
Ruiwen-T <you@example.com>
2021-10-06 06:36:57 -0400

    initsetup

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,28 @@
+/*
+Team Bernard Jr.: May Qiu (ducky: Perry Jr), Raven (Ruiwen) Tang (ducky: Bernard)
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / building on HW10 
+2021-10-06
+*/
+
+/* 
+DISCO
+- We could successfully compile BigSib.java with javac. Then, $ java BigSib yields the error: Main method not found in class BigSib, please define the main method as: public static void main(String[] args) or a JavaFX application class must extend javafx.application.Application. This discovery aligns with our expectations from class, as we learned that a class must have a main() method to be executable. We now also know that the compiler doesn’t necessarily check for a main() method.
+- We encountered the error: non-static variable helloMsg cannot be referenced from a static context. We were able to resolve this error by removing “static” from the greet and setHelloMsg methods.
+- Since a class is the blueprint for objects of the same type, we were able to use BigSib name = new BigSib() to initialize many objects of type BigSib.
+
+QCC
+- Why is there an error when referencing a non-static variable in a static method?
+- Are “//end main()” and “//end Greet” standard notation for labelling closing brackets?
+- Seems like one could have created multiple BigSibs, one for each line of output, or used richard for all lines and utilizing setHelloMsg and greet?
+*/
+
+public class BigSib{
+        String helloMsg;
+	public void setHelloMsg(String newMsg){
+		helloMsg = newMsg;
+	}
+	public String greet(String name){
+		return (helloMsg + " " + name);
+        }
+}
