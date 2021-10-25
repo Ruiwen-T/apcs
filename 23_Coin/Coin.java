2c69a887d1e6e6ed11cd189b81b91edd376e1faa
Ruiwen-T <you@example.com>
2021-10-25 00:35:27 -0400

    upload files

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -1,3 +1,23 @@
+/*
+Square Roots (Samantha Hua and Ruiwen (Raven) Tang)
+APCS
+HW23 -- What Does Equality Look Like?
+2021-10-23
+time spent: 0.7 hour
+DISCO
+- We should read ahead before we start coding in order to minimize confusion. 
+(For the toString() method we were trying to complete tasks that we are not capable of doing given the things we have learned in class.)
+- Even though we didn't use "this," we learned that it is used by an object to reference itself.
+- We realized we needed to set upFace to heads in all of the constructors in order for our code to behave the way we wanted it to.
+- If bias does not have a value set in the constructor, its value will automatically be 0 meaning that the probability of the coin flipping to heads would be 0 regardless of what Math.random() is.
+- If Math.random()*bias is not multiplied by two, the probability of the coin flipping to heads would still be 0. This is because the max value for Math.random() is 0.999 so when multiplied by 0.5(the value we set for bias), probability will never reach 0.5.
+QCC
+- Is it possible to call upon an object's name?
+	ex: ClassName ObjectName = new ClassName(); a function that would return ObjectName
+- Is there a more organized way to determine probability using bias? (especially when there are more than 2 options to choose from)
+- Does "Coin other" as an input refer to another object? How do we know which Object it is referring to? How can it reference an instance of a Class inside an instance of the same Class?
+*/
+
 /***
  *  class Coin
  *  by Clyde "Thluffy" Sinclair
@@ -21,7 +41,8 @@ public class Coin {
    *  postcond:
    ***/
   public Coin() {
-	
+	upFace = "heads";
+	bias = 0.5;
   }
 
 
@@ -38,6 +59,8 @@ public class Coin {
   ***/
   public Coin( String s ) {
 	name = s;
+	upFace = "heads";
+	bias = 0.5;
   }
 
 
@@ -49,32 +72,24 @@ public class Coin {
   public Coin( String s, String nowFace ) {
 	name = s;
 	upFace = nowFace;
+	bias = 0.5;
   }
 
 /* in order to compile our code 
   // Accessors...
   // ----------------------------
   public String getUpFace() {
-
   }
-
   public int getFlipCtr() {
 	
   }
-
   public double getValue() {
-
   }
-
   public int getHeadsCtr() {
-
   }
-
   public int getTailsCtr() {
-
   }
   // ----------------------------
-
 */
   /***
    * assignValue() -- set a Coin's monetary value based on its name
@@ -125,8 +140,9 @@ public class Coin {
    * Returns "heads" or "tails"
    ***/
   public String flip() {
-	double probability = Math.random()*bias;
-	if(bias >= 0.5){
+	double probability = 2*Math.random()*bias;
+// 	System.out.println(probability);
+	if(probability >= 0.5){
 		headsCtr += 1;
 		upFace = "heads";
 	}
@@ -161,7 +177,7 @@ public class Coin {
    * postcond: Return String comprised of name and current face
    ***/
   public String toString() {
-	return this.name + name + " -- " + upFace;
+	return name + " -- " + upFace;
   }
 
 }//end class
