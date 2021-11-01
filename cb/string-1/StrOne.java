bbf367de5baabe98ec07d5cd789ac13f53c4ce40
Ruiwen-T <you@example.com>
2021-10-31 22:12:25 -0400

    add test cases

--- cb/string-1/StrOne.java
+++ cb/string-1/StrOne.java
@@ -1,8 +1,24 @@
 public class StrOne{
-	public String helloName(String name){
+
+	/** Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+	**/
+	public static String helloName(String name){
 		return "Hello " + name + "!";
 	}
-	public String makeAbba(String a, String b) {
+
+	/** Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+	**/
+	public static String makeAbba(String a, String b) {
  		return a+b+b+a;
+	}
+
+	public static void main(String[] args){
+		System.out.println(helloName("Bob")); // → "Hello Bob!"
+		System.out.println(helloName("Alice")); // → "Hello Alice!"
+		System.out.println(helloName("X")); // → "Hello X!"
+	
+		System.out.println(makeAbba("Hi", "Bye")); // → "HiByeByeHi"
+		System.out.println(makeAbba("Yo", "Alice")); // → "YoAliceAliceYo"
+		System.out.println(makeAbba("What", "Up")); // → "WhatUpUpWhat"
 	}
 }
