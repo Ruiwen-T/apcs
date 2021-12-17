b4fb941382dc74089e94ade966cebd0adfad6580
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2021-12-17 05:17:49 -0500

    Update BinSearch.java

--- 48_search/BinSearch.java
+++ 48_search/BinSearch.java
@@ -1,3 +1,21 @@
+// Un-upTown Leopard Rock: Lea Kwok, Raven (Ruiwen) Tang
+// APCS pd7
+// HW48 -- Halving the Halves
+// 2021-12-16
+// time spent: 00.5 hours
+
+/* 
+DISCO:
+- For the isSorted() method, you can't use a foreach loop because you need to access the previous element, and there is no way to do that with a foreach loop.
+- You can use compareTo() for variable type Comparable since it's not a primitive. If the object it's being invoked on is greater than the argument, then compareTo() returns a number >0. If it is the opposite, it returns a number <0. If they are equal, then it returns 0.
+- Our recursive and iterative method bodies were very similar. The iterative one returned m instead of the method because it is not recursive.
+
+QCC:
+- We used the provided tPos variable as the value to return if the target was not in the Array, however, the skeleton describes it as being the return variable, so should we update its value? How would that help?
+- We had to be careful with using .equals() instead of == since we are not comparing primitives.
+- The binSearch() helper method lets us test both implementations since we don't have to put in so many parameters. binSearch() helper allows the user to use binSearch without knowing if it's recursive or iterative. 
+*/
+
 /**
    class BinSearch
    Binary search on array of Comparables
@@ -30,33 +48,55 @@ public class BinSearch
 
     int m = (lo + hi) / 2; //init mid pos var
 
+    if(lo <= hi){
 
+        if(a[m].equals(target)){
+            return m;
+        }
+        else if(a[m].compareTo(target) > 0){
+            hi = m-1;
+            return binSearchRec(a, target, lo, hi);
+        }
+        else {
+            lo = m+1;
+            return binSearchRec(a, target, lo, hi);
+        }
+    }
 
     return tPos;
   }//end binSearchRec
 
+    public static int binSearchIter( Comparable[] a,
+                                    Comparable target,
+                                    int lo, int hi )
+    {
 
-  public static int binSearchIter( Comparable[] a,
-                                   Comparable target,
-                                   int lo, int hi )
-  {
+        int tPos = -1; //init return var to flag value -1
+        int m = (lo + hi) / 2; //init mid pos var
 
-    int tPos = -1; //init return var to flag value -1
-    int m = (lo + hi) / 2; //init mid pos var
+        while(lo <= hi) { // run until lo & hi cross
 
-    while( /* ? */ ) { // run until lo & hi cross
+            //update mid pos var
+            m = (lo + hi) / 2;
 
-      //update mid pos var
+            // target found
+            if(a[m].equals(target)){
+                return m;
+            }
 
-      // target found
+            // value at mid index higher than target
+            else if(a[m].compareTo(target) > 0){
+                hi = m-1;
+            }
 
-      // value at mid index higher than target
+            // value at mid index lower than target
+            else {
+                lo = m+1;
+            }
 
-      // value at mid index lower than target
-
-    }
-    return tPos;
-  }//end binSearchIter
+        }
+        return tPos;
+    }//end binSearchIter
 
 
 
@@ -66,7 +106,7 @@ public class BinSearch
 
     boolean retBoo = true; //init to true, assume array is sorted
 
-    //Q: Why would a FOREACH loop not suffice here?
+    //Q: Why would a FOREACH loop not suffice here? A: You need to access the previous element, and there is no way to do that with a foreach loop.
     for( int i=0; i < arr.length-1; i++ ) {
       if ( ( arr[i].compareTo(arr[i+1]) > 0 ) ) {
         return false;
@@ -94,8 +134,7 @@ public class BinSearch
   //minimal -- augment as necessary
   public static void main ( String[] args )
   {
-
-    /*----------------------------------------------------
+ 
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     System.out.println("\nNow testing binSearch on Comparable array...");
@@ -103,38 +142,39 @@ public class BinSearch
     //Declare and initialize array of Comparables
     Comparable[] iArr = { 2, 4, 6, 8, 6, 42 };
     printArray( iArr );
-    System.out.println( "iArr1 sorted? -- " + isSorted(iArr) );
+    System.out.println( "iArr1 sorted? -- " + isSorted(iArr) ); //expected: false
 
     Comparable[] iArr2 = { 2, 4, 6, 8, 13, 42 };
     printArray( iArr2 );
-    System.out.println( "iArr2 sorted? -- " + isSorted(iArr2) );
+    System.out.println( "iArr2 sorted? -- " + isSorted(iArr2) ); //expected: true
 
     Comparable[] iArr3 = new Integer[10000];
     for( int i = 0; i < iArr3.length; i++ ) {
     iArr3[i] = i * 2;
     }
 
-    printArray( iArr3 );
-    System.out.println( "iArr3 sorted? -- " + isSorted(iArr2) );
+    //printArray( iArr3 );
+    System.out.println( "iArr3 sorted? -- " + isSorted(iArr3) );
 
     //search for 6 in array 
-    System.out.println( binSearch(iArr2,2) );
-    System.out.println( binSearch(iArr2,4) );
-    System.out.println( binSearch(iArr2,6) );
-    System.out.println( binSearch(iArr2,8) );
-    System.out.println( binSearch(iArr2,13) );
-    System.out.println( binSearch(iArr2,42) );
+    System.out.println( binSearch(iArr2,2) ); //expected: 0
+    System.out.println( binSearch(iArr2,4) ); //expected: 1
+    System.out.println( binSearch(iArr2,6) ); //expected: 2
+    System.out.println( binSearch(iArr2,8) ); //expected: 3
+    System.out.println( binSearch(iArr2,13) ); //expected: 4
+    System.out.println( binSearch(iArr2,42) ); //expected: 5
 
     //search for 43 in array 
-    System.out.println( binSearch(iArr2,43) );
+    System.out.println( binSearch(iArr2,43) ); //expected: -1
 
     System.out.println( "now testing binSearch on iArr3..." );
-    System.out.println( binSearch(iArr3,4) );
-    System.out.println( binSearch(iArr3,8) );
-    System.out.println( binSearch(iArr3,5) );
+    System.out.println( binSearch(iArr3,4) ); //expected: 2
+    System.out.println( binSearch(iArr3,8) ); //expected: 4
+    System.out.println( binSearch(iArr3,5) ); //expected: -1
 
     //search for 43 in array 
-    System.out.println( binSearch(iArr3,43) );
+    System.out.println( binSearch(iArr3,43) ); //expected: -1
+    /*----------------------------------------------------
     ====================================================*/
 
 
