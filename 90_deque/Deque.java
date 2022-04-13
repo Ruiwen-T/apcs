3f855ae06ea0a8cafc4ee742a9fb67172c0255d7
RuiwenT <ruiwentang@gmail.com>
2022-04-12 21:33:02 -0400

    + hw90

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,7 +1,14 @@
+/*
+Mr. K: May Qiu, Raven (Ruiwen) Tang, Kaitlin Ho
+APCS pd6
+HW90 -- Swabbing the Deque / implementing Deque in compliance with interface
+2022-04-12
+time spent: 1.0 hr
+*/
+
 public interface Deque<Card>{
 	public int size();
 
-	/*
 	public void addLast(Card x);
 
 	public void addFirst(Card x);
@@ -13,5 +20,6 @@ public interface Deque<Card>{
 	public Card removeFirst();
 
 	public Card removeLast();
+	/*
 	*/
 }
